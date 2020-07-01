838bd29ba7d811a7c44e4805315d4b0c7fdc8c31
Mehran028 <Mehran028>
2020-07-02 01:23:56 +0500

    HTML commit

---
+++ Registration Form.js
@@ -0,0 +1,16 @@
+  <script type = "text/javascript">
+
+function Application(){
+
+alert("Application Accepted");
+
+}
+
+function Registration(){
+
+alert("Exit");
+}
+</script>
+<input type = "button"  onclick = "Application();" name = "OK"  value = "Submit" />
+
+<input type = "button"  onclick = "Registration();" name = "Ok" value = "Exit" />
